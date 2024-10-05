49abf5dc20cd3c0a4870e536df287853db311d9a
ksobczykR2DS <ksobczyk@student.agh.edu.pl>
2024-10-05 11:38:22 +0200

    adding sequence to  smile dict

---
+++ sequence_to_smiles_dict.py
@@ -0,0 +1,22 @@
+SMILES_DICT = {
+        'A': 'CC(N)C(=O)O',  # Alanine
+        'R': 'NC(CCCNC(=N)N)C(=O)O',  # Arginine
+        'N': 'NC(CC(=O)N)C(=O)O',  # Asparagine
+        'D': 'NC(CC(=O)O)C(=O)O',  # Aspartic acid
+        'C': 'NC(CS)C(=O)O',  # Cysteine
+        'E': 'NC(CCC(=O)O)C(=O)O',  # Glutamic acid
+        'Q': 'NC(CCC(=O)N)C(=O)O',  # Glutamine
+        'G': 'NCC(=O)O',  # Glycine
+        'H': 'NC(CC1=CN=CN1)C(=O)O',  # Histidine
+        'I': 'CC(C)C(N)C(=O)O',  # Isoleucine
+        'L': 'CC(C)CC(N)C(=O)O',  # Leucine
+        'K': 'NCCCCC(N)C(=O)O',  # Lysine
+        'M': 'CSCCC(N)C(=O)O',  # Methionine
+        'F': 'NC(CC1=CC=CC=C1)C(=O)O',  # Phenylalanine
+        'P': 'OC(=O)C1CCCN1',  # Proline
+        'S': 'NC(CO)C(=O)O',  # Serine
+        'T': 'CC(O)C(N)C(=O)O',  # Threonine
+        'W': 'NC(CC1=CN=C2C1=CC=CC2)C(=O)O',  # Tryptophan
+        'Y': 'NC(CC1=CC=C(O)C=C1)C(=O)O',  # Tyrosine
+        'V': 'CC(C)C(N)C(=O)O'  # Valine
+    }
